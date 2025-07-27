06ff063106c0c44522b5f1d0c864b9c1227301ab
HaroldRobson <wt23188@bristol.ac.uk>
2025-07-27 12:16:42 +0100

    Update Factory2.sol

--- MAIN/smartcontracts/VCC_ledger/src/Factory2.sol
+++ MAIN/smartcontracts/VCC_ledger/src/Factory2.sol
@@ -98,15 +98,5 @@ function stringToBytes32(string memory source) public pure returns (bytes32 resu
     }
 }
 
-function stringToBytes32(string memory source) public pure returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
-
-        assembly {
-            result := mload(add(source, 32))
-        }
-    }
 
 }
